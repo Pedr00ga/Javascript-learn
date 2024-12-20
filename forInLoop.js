e2518418b5267762f4420b1e05789463a9aa4318
Pedr00ga <plgonzales4760@gmail.com>
2024-12-19 21:08:58 -0300

    Aula 19/12

---
+++ forInLoop.js
@@ -0,0 +1,9 @@
+
+const meuCarro = {
+    model: "Mercedes",
+    ano: 2025,
+    km: 1250
+}
+
+for (let i in meuCarro)
+    console.log(i, meuCarro[i])
---
+++ whileLoop.js
@@ -0,0 +1,8 @@
+
+i = 1
+
+while (i <= 1000){
+    if (i === 25) break;
+    console.log("Numero" , i);
+    i++
+}
